38196581e3e529ec2f14a8910e5eaebef93c8811
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-27 15:47:54 +0800

    add page views

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,7 +1,52 @@
 class AnimalsController < ApplicationController
+
   def index
     @animals = Animal.all
   end
 
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      flash[:notice] = "Specimen successfully added!"
+      redirect_to  animals_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      flash[:notice] = "Name updated successfully!"
+      redirect_to animals_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    flash[:notice] = "Specimen successfully deleted!"
+    redirect_to animals_path
+  end
+
+  private
+
+    def animal_params
+      params.require(:animal).permit(:name)
+    end
 
 end
